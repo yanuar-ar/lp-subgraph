d299c6593d319c3055d798e97c0e37daf70553ff
yanuar-ar <yanuar.ar@gmail.com>
2022-12-08 08:46:34 +0700

    init

---
+++ src/the-lp.ts
@@ -0,0 +1,153 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  ConsecutiveTransfer as ConsecutiveTransferEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  PaymentReceived as PaymentReceivedEvent,
+  PaymentReleased as PaymentReleasedEvent,
+  Transfer as TransferEvent,
+  SellCall,
+  BuyCall,
+} from "../generated/TheLP/TheLP";
+import {
+  Approval,
+  ApprovalForAll,
+  ConsecutiveTransfer,
+  OwnershipTransferred,
+  PaymentReceived,
+  PaymentReleased,
+  Transfer,
+  Sell,
+  Buy,
+} from "../generated/schema";
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleConsecutiveTransfer(
+  event: ConsecutiveTransferEvent
+): void {
+  let entity = new ConsecutiveTransfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.fromTokenId = event.params.fromTokenId;
+  entity.toTokenId = event.params.toTokenId;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.user = event.params.user;
+  entity.newOwner = event.params.newOwner;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handlePaymentReceived(event: PaymentReceivedEvent): void {
+  let entity = new PaymentReceived(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.from;
+  entity.amount = event.params.amount;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handlePaymentReleased(event: PaymentReleasedEvent): void {
+  let entity = new PaymentReleased(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+// =====================================
+
+export function handleSell(call: SellCall): void {
+  let entity = new Sell(call.transaction.hash);
+
+  entity.from = call.transaction.from;
+  entity.tokenId = call.inputs.tokenId;
+  entity.blockNumber = call.block.number;
+  entity.blockTimestamp = call.block.timestamp;
+
+  entity.save();
+}
+
+export function handleBuy(call: BuyCall): void {
+  let entity = new Buy(call.transaction.hash);
+
+  entity.from = call.transaction.from;
+  entity.tokenId = call.inputs.id;
+  entity.blockNumber = call.block.number;
+  entity.blockTimestamp = call.block.timestamp;
+
+  entity.save();
+}
---
+++ tests/the-lp-utils.ts
@@ -0,0 +1,171 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  ConsecutiveTransfer,
+  OwnershipTransferred,
+  PaymentReceived,
+  PaymentReleased,
+  Transfer
+} from "../generated/TheLP/TheLP"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createConsecutiveTransferEvent(
+  fromTokenId: BigInt,
+  toTokenId: BigInt,
+  from: Address,
+  to: Address
+): ConsecutiveTransfer {
+  let consecutiveTransferEvent = changetype<ConsecutiveTransfer>(newMockEvent())
+
+  consecutiveTransferEvent.parameters = new Array()
+
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam(
+      "fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(fromTokenId)
+    )
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam(
+      "toTokenId",
+      ethereum.Value.fromUnsignedBigInt(toTokenId)
+    )
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  consecutiveTransferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+
+  return consecutiveTransferEvent
+}
+
+export function createOwnershipTransferredEvent(
+  user: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createPaymentReceivedEvent(
+  from: Address,
+  amount: BigInt
+): PaymentReceived {
+  let paymentReceivedEvent = changetype<PaymentReceived>(newMockEvent())
+
+  paymentReceivedEvent.parameters = new Array()
+
+  paymentReceivedEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  paymentReceivedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return paymentReceivedEvent
+}
+
+export function createPaymentReleasedEvent(
+  to: Address,
+  amount: BigInt
+): PaymentReleased {
+  let paymentReleasedEvent = changetype<PaymentReleased>(newMockEvent())
+
+  paymentReleasedEvent.parameters = new Array()
+
+  paymentReleasedEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  paymentReleasedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return paymentReleasedEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
